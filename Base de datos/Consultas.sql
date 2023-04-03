692c9d029927ae5ef93672a7893213b9fee5a1e0
wilmarfra2017 <wilframar@gmail.com>
2023-04-03 08:55:52 -0500

    Update Consultas.sql

--- Base de datos/Consultas.sql
+++ Base de datos/Consultas.sql
@@ -17,25 +17,3 @@ JOIN
   editoriales ON libros.editoriales_id = editoriales.id
 
 ------------------------------------------------------------------
-
--------------------------------------------------------
-
-SELECT 
-  libros.ISBN,
-  libros.titulo,
-  autores.nombre AS autor_nombre,
-  autores.apellidos AS autor_apellidos,
-  editoriales.nombre AS editorial_nombre,
-  editoriales.sede AS editorial_sede,
-  libros.sinopsis,
-  libros.n_paginas
-FROM
-  libros
-JOIN
-  autores_has_libros ON libros.ISBN = autores_has_libros.libros_ISBN
-JOIN
-  autores ON autores_has_libros.autores_id = autores.id
-JOIN
-  editoriales ON libros.editoriales_id = editoriales.id
-
-WHERE Libros.titulo LIKE '%ciudad%'
